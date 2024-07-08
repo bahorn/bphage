b53a4248ee341d82669e1d997ec727eec646f95d
B Horn <b@horn.uk>
2024-07-08 10:02:36 +0100

    Updating comments, making it a better writeup
    Signed-off-by: B Horn <b@horn.uk>

--- src/bphage.asm
+++ src/bphage.asm
@@ -24,22 +24,17 @@
 ; * send a HTTP request for /5/5
 ; * and write() out the contents to stdout
 ;
-; You might be wondering why we'd go through the effort, when we could just
+; You might be wondering why we would go through the effort, when we could just
 ; do dynamic linking normally and not bother patching?
 ;
 ; The idea was that dynamic linking normally required a pretty hefty amount of
 ; headers (prior work[1] got 32bit ELFs down to 300 bytes, resolving only
-; `exit()`, which is also out of date as you'd need to use GNU_HASH this time
-; due to recent glibc changes.). And you'd still need to do setup glibc.
+; `exit()`, which is also out of date as you'd probably now need to use GNU_HASH
+; this time due to recent glibc changes). And you'd still need to do setup 
+; glibc to be able to use dlopen and libssl.
 ;
 ; I hope this is interesting, was my first time golfing a binary. Messed up
 ; my sleeping pattern hacking on this, listening to Chappell Roan on repeat!
-;
-; A quick note on distro support is that this works on Ubuntu and Fedora.
-; Debian can work, it just requires changing on offset (as the docker container
-; I was looking at doesn't have endbr64 instructions)
-; Arch Linux will not work, compiled with a different relocation type that is
-; not supported in this.
 ;
 ; - bah
 ;
@@ -48,6 +43,18 @@
 ; +---------------------------------------------------------------------------+
 ; |An overview                                                                |
 ; +---------------------------------------------------------------------------+
+;
+; So to implement our patch we need to find where `main()` in `/bin/bash` is,
+; and a way of resolving `dlopen()` and `dlsym()`.
+; To find `main()`, a simple trick of using a known offset from `_start` was
+; used, and finding the other two symbols involves looking at the .dynamic
+; section to find values we need to walk the relocation table.
+;
+; For distro support, this is known to work on Ubuntu and Fedora.
+; Debian can work, it just requires changing an offset (as the docker container
+; I was looking at doesn't have endbr64 instructions)
+; Arch Linux will not work, compiled with a different relocation type that is
+; not supported by this.
 ;
 ; To summerize some of my code size optimization approaches:
 ; * use 32bit/16bit/8bit versions of registers where it made sense and lowered
@@ -69,14 +76,35 @@
 ; ideas.
 ;
 ; For checking instruction size, I mainly used the good old defuse.ca web 
-; assembler, but i gather netspooky has a pretty nice repl called scare which is
-; probably a better choice.
+; assembler, but netspooky has a pretty nice repl called scare which is probably
+; a better choice, will be what I use going forward.
 ;
 ; Feel free to try and improve this! I'd love to see if actual dynamic linking
-; could work (I think it might be viable on 32bit at least) and some compression
-; for the patch.
+; could work (I think it might be viable on 32bit at least), it'll at least
+; probably fit in the 4k limit, and some compression for the patch.
+;
+; I did investigate doing compression, was working on something similar to the
+; earlier lz compressors, with a bit more of an explicit dictionary, which can
+; be implemented in around 40 bytes or so.
+; The idea was to have a dictionary, then a list of that would index into the
+; dictionary and say how much to read.
+; But, my efforts to find a suitable dictionary to use is where that plan
+; failed, as it ended up gaining 20 bytes or so when we added the decompressor
+; code and the list.
+;
+; I tried using a ILP solver to generate an optimal list given a dictionary, but
+; was not able to get the amount of savings I needed. Maybe with a better
+; dictionary this could work better, but I'm not sure about the optimal way of
+; constructing it.
+;
+; You'd probably need a better compression algorithm (gzip goes down to 260 or
+; so bytes) but problem of dealing with the decompression code alongside that.
+; Maybe if I was demoscening I'd do two stages and just import zlib.
+;
+; You can see my tests there in my repo, under `old/compression-experiments` [3]
 ;
 ; [2] https://codegolf.stackexchange.com/questions/132981/
+; [3] https://github.com/bahorn/bphage/tree/d33b5271bbd1f9c6e9ecdb0232398edca3d74867/old/compression-experiments
 BITS 64
 
 ; The syscalls we need.
@@ -95,22 +123,22 @@ BITS 64
 %macro  rslvsym 2
         pop     rsi
         push    rsi
-        add     rsi, %2 - _str_libssl
         regcopy rdi, %1
+        add     rsi, %2 - _str_libssl
         call    _dlsym
 %endmacro
 
 ; +---------------------------------------------------------------------------+
 ; |The ELF header                                                             |
 ; +---------------------------------------------------------------------------+
-; I started off with the header from [3] by Brian Raiter, licensed under the
+; I started off with the header from [4] by Brian Raiter, licensed under the
 ; GPL 2 or later.
 ;
 ; The primary changes I made were replacing the start code to store the string
 ; "/bin/bash", and used some free bytes I saw being used in another entry to
 ; do a syscall and save 2 bytes overall.
 ;
-; [3] https://www.muppetlabs.com/~breadbox/software/tiny/tiny-x64.asm.txt
+; [4] https://www.muppetlabs.com/~breadbox/software/tiny/tiny-x64.asm.txt
         org     0x500000000
 
         db      0x7F                    ; e_ident
@@ -136,9 +164,9 @@ phdr:
 ; 6 bytes we can use, down to 4 because of the jump we need to do, as there is
 ; no benefit from using it at the end, as that will require a long jump making
 ; the savings pointless.
-; I learnt this trick from reading mndz's entry [4].
+; I learnt this trick from reading mndz's entry [5].
 ;
-; [4] https://github.com/0x6d6e647a/bggp-2024/blob/main/elf64.asm
+; [5] https://github.com/0x6d6e647a/bggp-2024/blob/main/elf64.asm
 _header_save:
         add     al, SYS_open
         syscall
@@ -221,11 +249,13 @@ _find_dynamic_loop:
 ; d_val.
 ; d_tag is just the name we are looking for, with d_val being the value.
 ; So through this we want to find the offsets for the strtab (so we can verify
-; the name of a symbol), symtab (to get check the name) and the JMPREL
-; relocations.
+; the name of a symbol), symtab (to get the name) and the JMPREL relocations.
 ;
 ; JMPREL will point to a symbol, and a symbol will point to an offset in the
 ; strtab, so we need all of those to check the name for a relocation.
+;
+; Not every distro will have JMPREL as it depends on how bash was compiled, so
+; this is potentially one of the ways this can fail.
 ;
 ; The relocations we find can then be used to get the address that a pointer to
 ; the target function will be written to.
@@ -272,11 +302,11 @@ _read_sht_dynamic_tail:
 ; targets.
 ;
 ; I came up with the trick involving checking just some bytes from the target
-; symbol myself, but when I was reading through the writeup by Amethyst [5] I 
-; found out netspooky used a similar trick in BGGP2.
+; symbol myself, but when I was reading through the writeup by Amethyst Basilisk
+; [6] I found out netspooky used a similar trick in BGGP2.
 ; Though I have to use 4 bytes instead of two here.
 ;
-; [5] https://amethyst.systems/blog/posts/entry-for-bggp5/
+; [6] https://amethyst.systems/blog/posts/entry-for-bggp5/
 ;
 ; register usage:
 ; * rbp - strtab_offset
@@ -352,11 +382,15 @@ _process_relocs_loop_tail:
 ; However, there is a slight complication as some distros like debian (really
 ; its just debian) seem to compile bash with -fcf-protection=none, at least in
 ; docker.
-; This results in the endbr64 instruction not being included
+; This results in the endbr64 instruction not being included, so the offset
+; needs to be adjusted.
+;
+; Older ubuntu also has a different offset, but requires a different openssl
+; version to be used.
 _discover_main:
         ; useful offsets for discovering main()
         %assign e_entry_offset 24
-        ; e_entry + 27 is the lea we want.
+        ; e_entry + 27 is the `lea` we want.
         ; needs to 24 on debian
         %assign main_offset 27
         ; 31 is the rip offset we need
@@ -382,7 +416,7 @@ _discover_main:
 ; |Applying the patches                                                       |
 ; +---------------------------------------------------------------------------+
 ; So, to patch the binary we want to copy our code into main(), which we'll use
-; our previously obtained offset for and memcpy it in.
+; our previously obtained offset and memcpy it in.
 ;
 ; Then we just need to patch the jump table we have at the start of the code to
 ; contain references to dlopen() and dlsym(), so we can freely use those in our
@@ -417,7 +451,7 @@ _setup_memfd:
         ; copying rdi to rbx so we can reuse it later on.
         regcopy rbx, rdi
 
-; This one requires a bit of an explaination.
+; This one requires a bit of an explanation.
 ;
 ; write() takes its count (size_t) in (r|e)dx, and can write up to that or up to
 ; 0x7ffff000 bytes.
@@ -456,7 +490,7 @@ _execve_memfd:
         jmp     _step_1
 
 ; we will now be in the patch after the execveat(), so lets move onto that!
-; We slightly overly code as we have some extra bytes avaliable in the jump
+; We slightly overly code as we have some extra bytes available in the jump
 ; tables.
 _patch_start:
         jmp     _patch_code
@@ -485,7 +519,7 @@ _dlsym_end:
 ; +---------------------------------------------------------------------------+
 ; |What are we implementing?                                                  |
 ; +---------------------------------------------------------------------------+
-; To give a rougth explaination of our patch, here is the C version I wrote to
+; To give a rough explanation of our patch, here is the C version I wrote to
 ; figure out all the calls I needed to make:
 ; +---------------------------------------------------------------------------+
 ; |   1   │ #include <unistd.h>                                               |
@@ -515,7 +549,7 @@ _dlsym_end:
 ; |  25   │ }                                                                 |
 ; +---------------------------------------------------------------------------+
 ; Overall, pretty simple. `BIO_ctrl()` sits behind a macro when you follow
-; tutorials, but thats what setting the hostname calls under the hood.
+; tutorials, but that is what setting the hostname calls under the hood.
 ; It'll malloc everything by itself, so as long as we are fine trashing the
 ; stack we don't need to really do any other memory allocations.
 ; 
@@ -523,7 +557,7 @@ _dlsym_end:
 ; restricted which registers I could use, as many registers get trashed when we
 ; call into libssl and libc.
 ;
-; So throughout this code I primaily used the following registers:
+; So throughout this code I primarily used the following registers:
 ; * rsp - buffer - we are just trashing the stack to store the request.
 ; * rbx - libssl handle
 ; * rbp - BIO_read, scratch
@@ -561,10 +595,10 @@ _patch_code:
         %assign BIO_C_SET_CONNECT 0x64
 
         rslvsym rbx, _str_BIO_ctrl
+        regcopy rdi, rbp
         lea     rcx, [rel _str_host]
         xor     edx, edx
         mov     sil, BIO_C_SET_CONNECT
-        regcopy rdi, rbp
         call    rax
 
         rslvsym rbx, _str_BIO_puts
@@ -606,7 +640,7 @@ _patch_code:
         syscall
 
 ; Don't want to crash and exit() requires far more code and we have completely
-; trashed large amounts of code so not safe to return.
+; trashed the stack and large amounts of code so not safe to return.
 _inf:
         jmp     _inf
 
@@ -655,7 +689,45 @@ _str_host:
         db      "binary.golf:443", 0
 _patch_end:
 ;
-; *insert hexdump here when ready for submission*
+; 00000000: 7f45 4c46 eb42 2f62 696e 2f62 6173 6800  .ELF.B/bin/bash.
+; 00000010: 0200 3e00 0100 0000 0100 0000 0500 0000  ..>.............
+; 00000020: 1800 0000 0000 0000 1800 0000 0500 0000  ................
+; 00000030: 0402 0f05 eb22 3800 0100 4000 0000 0000  ....."8...@.....
+; 00000040: 0100 4000 0000 0000 4881 ec00 0050 0048  ..@.....H....P.H
+; 00000050: 8d3d b0ff ffff ebd8 ba00 0050 0054 5e97  .=.........P.T^.
+; 00000060: 930f 058b 4424 2883 c040 837c 0404 0675  ....D$(..@.|...u
+; 00000070: f68b 5c04 188b 341c 8b7c 1c08 83fe 050f  ..\...4..|......
+; 00000080: 44ef 83fe 060f 44c7 83fe 170f 44cf 83c3  D.....D.....D...
+; 00000090: 1085 f675 e08b 7c0c 0c6b ff18 01c7 8b1c  ...u..|..k......
+; 000000a0: 3c01 eb81 3c1c 646c 6f70 440f 4434 0c81  <...<.dlopD.D4..
+; 000000b0: 3c1c 646c 7379 440f 443c 0c83 c118 4d85  <.dlsyD.D<....M.
+; 000000c0: ff74 d24d 85f6 74cd 8b44 2418 505b 83c0  .t.M..t..D$.P[..
+; 000000d0: 1b48 6304 0404 1f01 c353 5a48 01e2 b943  .Hc......SZH...C
+; 000000e0: 0100 0048 8d35 3f00 0000 525f f3a4 83c3  ...H.5?...R_....
+; 000000f0: 0849 29de 83c3 0649 29df 4489 7204 4489  .I)....I).D.r.D.
+; 00000100: 7a0a 31f6 488d 3d04 ffff ffb8 3f01 0000  z.1.H.=.....?...
+; 00000110: 0f05 575b f7da 545e 97b0 010f 0566 41b8  ..W[..T^.....fA.
+; 00000120: 0010 b842 0100 00eb 04eb 0cff 2553 5eeb  ...B........%S^.
+; 00000130: 02ff 2531 d20f 0531 f6ff c648 8d3d 9d00  ..%1...1...H.=..
+; 00000140: 0000 57e8 e3ff ffff 505b 5e56 535f 4883  ..W.....P[^VS_H.
+; 00000150: c60c e8da ffff ffff d050 5d5e 5653 5f48  .........P]^VS_H
+; 00000160: 83c6 1ee8 c9ff ffff 555f ffd0 505d 5e56  ........U_..P]^V
+; 00000170: 535f 4883 c62a e8b6 ffff ff55 5fff d050  S_H..*.....U_..P
+; 00000180: 5d5e 5653 5f48 83c6 3ee8 a3ff ffff 555f  ]^VS_H..>.....U_
+; 00000190: 488d 0dc5 0000 0031 d240 b664 ffd0 5e56  H......1.@.d..^V
+; 000001a0: 535f 4883 c647 e886 ffff ff48 8d35 8600  S_H..G.....H.5..
+; 000001b0: 0000 555f ffd0 5e56 535f 4883 c650 e86e  ..U_..^VS_H..P.n
+; 000001c0: ffff ff55 5f50 5d54 5e57 5666 92ff d55e  ...U_P]T^WVf...^
+; 000001d0: 5f92 ffd5 9254 5eb0 0189 c70f 05eb fe6c  _....T^........l
+; 000001e0: 6962 7373 6c2e 736f 2e33 0054 4c53 5f63  ibssl.so.3.TLS_c
+; 000001f0: 6c69 656e 745f 6d65 7468 6f64 0053 534c  lient_method.SSL
+; 00000200: 5f43 5458 5f6e 6577 0042 494f 5f6e 6577  _CTX_new.BIO_new
+; 00000210: 5f73 736c 5f63 6f6e 6e65 6374 0042 494f  _ssl_connect.BIO
+; 00000220: 5f63 7472 6c00 4249 4f5f 7075 7473 0042  _ctrl.BIO_puts.B
+; 00000230: 494f 5f72 6561 6400 4745 5420 2f35 2f35  IO_read.GET /5/5
+; 00000240: 2048 5454 502f 312e 310a 486f 7374 3a62   HTTP/1.1.Host:b
+; 00000250: 696e 6172 792e 676f 6c66 0a0a 6269 6e61  inary.golf..bina
+; 00000260: 7279 2e67 6f6c 663a 3434 3300            ry.golf:443.
 ;
 ; enj0y!
 ; EOT
